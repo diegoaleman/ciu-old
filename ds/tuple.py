eca44cc42a801fd98f64e6b470a828e081829c38
Diego Aleman <diegoalemtz@gmail.com>
2018-11-05 15:42:09 -0600

    tuple file

---
+++ ds/tuple.py
@@ -0,0 +1,19 @@
+# create tuple
+t1 = (1,'diego','bear',1993)
+print(t1)
+
+# access tuple
+index_1_element = t1[1]
+
+# update tuple (NOT POSSIBLE)
+# t1[1] = 'maria'
+
+# delete element (NOT POSSIBLE)
+# del t1[1]
+
+# operations
+size = len(l)       # O(1)
+concat = (1,2,3) + (4,5,6)
+mult = (1,2,3) * 3
+memb = 3 in concat      # O(n)
+
