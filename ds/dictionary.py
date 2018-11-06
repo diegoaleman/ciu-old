987517c01233e9eab9d19e74a982e06fa28eaf97
Diego Aleman <diegoalemtz@gmail.com>
2018-11-06 13:47:29 -0600

    chainmaps section to dicts

--- ds/dictionary.py
+++ ds/dictionary.py
@@ -25,3 +25,32 @@ values = d1.values()
 # items - return tuple with key:value pairs
 items = list(d1.items())    # has to be casted to list, returns view not list
 first_pair = items[0]
+
+
+
+# Chain Maps
+# manage multiple dicts as one unit
+# eliminates duplicate keys
+# main use is to search multiple dicts at the same time
+import collections
+
+dict1 = {'day1': 'Mon', 'day2': 'Tue'}
+dict2 = {'day3': 'Wed', 'day1': 'Thu'}
+
+res = collections.ChainMap(dict1, dict2)
+
+dict_keys = list(res.keys())
+dict_values = list(res.values())
+
+for key,val in res.items():
+    print(key + "-" + val)
+
+value_in_dict_1 = 'day1' in res
+value_in_dict_2 = 'day5' in res
+
+res['day3'] = 'Fri'
+res['day1'] = 'Sat'
+dict1['day2'] = 'Sun'
+
+print(res.maps, '\n')
+
