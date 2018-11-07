fedae7c76d66be074e7fcca7d5eda3c6890098cf
Diego Aleman <diegoalemtz@gmail.com>
2018-11-06 19:16:29 -0600

    skeleton for doubly linked list implementation

---
+++ ds/doubly_linked_list.py
@@ -0,0 +1,19 @@
+class Node:
+    def __init__(self,d=None):
+        self.data = d
+        self.next = None
+        self.prev = None
+
+# TODO
+# - push_front(value)
+# - pop_front
+# - push_back(value)
+# - pop_back
+# - insert(index, value)
+# - erase(index)
+
+class DoublyLinkedList(self):
+    def __init__(self):
+        self.head = None
+        self.size = 0
+
