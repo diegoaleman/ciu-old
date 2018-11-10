bf2dc37471ee556fe2cb58edb2e95fb296a9bf1e
Diego Aleman <diegoalemtz@gmail.com>
2018-11-10 00:12:27 -0600

    implement insert and tree traversals

--- ds/bst.py
+++ ds/bst.py
@@ -23,3 +23,62 @@ class BST:
     def __init__(self):
         self.head = None
 
+    def insert(self, data):
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+        else:
+            inserted = False
+            it = self.head
+            while not inserted:
+                if data > it.data:
+                    if it.right is None:
+                        it.right = new_node
+                        inserted = True
+                    else:
+                        it = it.right
+                elif data < it.data:
+                    if it.left is None:
+                        it.left = new_node
+                        inserted = True
+                    else:
+                        it = it.left
+                else:   # if inserted value already exists
+                    return 0
+
+    def print(self, option):
+        if option == 1:
+            self.preorder(self.head)
+        elif option == 2:
+            self.inorder(self.head)
+        else:
+            self.postorder(self.head)
+        print()
+
+    def preorder(self,node):
+        if node is not None:
+            print(node.data, end=' ')
+            self.preorder(node.left)
+            self.preorder(node.right)
+
+    def inorder(self,node):
+        if node is not None:
+            self.inorder(node.left)
+            print(node.data, end=' ')
+            self.inorder(node.right)
+
+    def postorder(self,node):
+        if node is not None:
+            self.postorder(node.left)
+            self.postorder(node.right)
+            print(node.data, end=' ')
+
+
+bst = BST()
+bst.insert(8)
+bst.insert(4)
+bst.insert(10)
+bst.insert(6)
+bst.print(1)
+bst.print(2)
+bst.print(3)
