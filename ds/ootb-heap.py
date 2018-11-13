d26593d11ef06d3988dbd146c94279e1cf07ca5c
Diego Aleman <diegoalemtz@gmail.com>
2018-11-12 19:02:42 -0600

    heapq with priority number and tuple object

--- ds/ootb-heap.py
+++ ds/ootb-heap.py
@@ -18,3 +18,16 @@ heapq.heappush(h,1)
 # remove
 min_item = h[0]
 heapq.heappop(h)
+
+
+
+# now, with objects
+h2 = []
+heapq.heapify(h2)
+heapq.heappush(h2,(24,'Diego'))
+heapq.heappush(h2,(21,'Paula'))
+heapq.heappush(h2,(15,'Fer'))
+
+# retuns tuple with lowest value
+min_elem2= h2[0]
+
