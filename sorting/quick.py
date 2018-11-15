fce18fc257083651a2cf0c836cf6ad8a79eace92
Diego Aleman <diegoalemtz@gmail.com>
2018-11-14 19:46:59 -0600

    quick sort implementation

---
+++ sorting/quick.py
@@ -0,0 +1,34 @@
+# Quick sort
+# best case -> O(nlogn)
+# worst case -> O(n^2)
+def quicksort(list,start,end):
+    if start<end:
+        index = partition(list,start,end)
+        quicksort(list,start,index-1)
+        quicksort(list,index+1,end)
+
+def partition(list,start,end):
+    pivot = list[start]
+    left = start + 1
+    right = end
+    done = False
+
+    while not done:
+        while left <= right and list[left] <= pivot:
+            left+=1
+
+        while left <= right and list[right] >= pivot:
+            right-=1
+
+        if left < right:
+            list[left],list[right] = list[right],list[left]
+        else:
+            done = True
+    list[start],list[right] = list[right],list[start]
+    return right
+
+
+l = [7,2,6,9,3,5,1,8]
+print(l)
+quicksort(l,0,len(l)-1)
+print(l)
