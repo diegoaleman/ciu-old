a547493801d92f9f04c303227b983bce91c62a50
Diego Aleman <diegoalemtz@gmail.com>
2018-11-16 14:19:07 -0600

    skeleton and todo for graph adj list

---
+++ ds/graph-adj-list.py
@@ -0,0 +1,15 @@
+# Graph
+
+# vertex -> node in a graph
+# edge -> connects two vertices
+# weight -> cost of going from one vertex to another
+# path -> sequence of vertices connected by edges
+# cycle -> a path that starts and ends in the same vertex
+
+# TODO
+# - addVertex(vert) -> adds new vertex to graph
+# - addEdge(fromVert, toVert) -> adds directed edge
+# - addEdge(fromVert, toVert, weight) -> adds weighted directed edge
+# - getVertex(vertKey) -> finds vertex with key name
+# - getVertices() -> get list of vertices in graph
+# - in -> boolean to check if vertex is in graph
