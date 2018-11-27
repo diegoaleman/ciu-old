970a4878e50f88541475eb2c73e31fd7dd03c8ad
Diego Aleman <diegoalemtz@gmail.com>
2018-11-27 13:59:17 -0600

    kruskal with cycle detection finished

--- algorithms/greedy-kruskal.py
+++ algorithms/greedy-kruskal.py
@@ -1,24 +1,50 @@
 # Kruskal algorithm
 import heapq
 
-def initialValues(matrix,length,h):
+def initHeap(matrix,length,h):
     for i in range(length):
         for j in range(length):
             val = matrix[i][j]
             if val > 0:
                 heapq.heappush(h,(val,[i,j]))
 
+def setUnion(parent,x,y):
+    x_set = findParent(parent,x)
+    y_set = findParent(parent,y)
+
+    parent[x_set] = y_set
+
+def findParent(parent, index):
+    if parent[index] == -1:
+        return index
+    if parent[index] != -1:
+        return findParent(parent,parent[index])
+
+def validNode(parent,x,y):
+    x_set = findParent(parent,x)
+    y_set = findParent(parent,y)
+
+    if x_set == y_set:
+        return False
+    else:
+        setUnion(parent,x,y)
+        return True
+
 def kruskal(matrix, length):
     h = []
-    heapq.heapify(h)
-    initialValues(matrix,length,h)
-    visited = set()
     result = []
+    visited = set()
+    initHeap(matrix,length,h)
     node = heapq.heappop(h)
+    valid_node = True
+    parent = [-1] * length
+    setUnion(parent,node[1][0], node[1][1])
 
     while len(h) > 0:
-        while node[1][1] in visited and len(h) > 0:
+        while not valid_node and len(h) > 0:
             node = heapq.heappop(h)
+            valid_node = validNode(parent,node[1][0],node[1][1])
+        valid_node = not valid_node
 
         if len(h) == 0 and node[1][1] in visited:
             return result
@@ -26,8 +52,8 @@ def kruskal(matrix, length):
         visited.add(node[1][0])
         visited.add(node[1][1])
         result.append(node)
-    return result
 
+    return result
 
 def main():
     matrix = [
@@ -41,6 +67,7 @@ def main():
     n = 7
     res = kruskal(matrix,n)
     print(res)
+
 
 if __name__ == "__main__":
     main()
