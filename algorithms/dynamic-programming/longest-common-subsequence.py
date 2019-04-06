ac40810d1bf4d6a9e3efd501f0d1d8629d690bb7
Diego Aleman <diegoalemtz@gmail.com>
2019-04-05 18:16:54 -0600

    LCS finished

--- algorithms/dynamic-programming/longest-common-subsequence.py
+++ algorithms/dynamic-programming/longest-common-subsequence.py
@@ -1,6 +1,3 @@
-# ABCDGH
-# AEDFHR
-# ADH
 def LCS_recursive(s1,s2):
     if s1 == '' or s2 == '':
         return 0
@@ -9,16 +6,21 @@ def LCS_recursive(s1,s2):
     else:
         return max(LCS_recursive(s1[:len(s1)-1],s2), LCS_recursive(s1, s2[:len(s2)-1]))
 
-
-def LCS_dynamic(s1,s2):
-    matrix = [[0]*(len(s1)+1)]*(len(s2)+1)
-    return matrix
+def LCS_dynamic(s1,s2,matrix):
+    for row in range(1,len(s2)+1):
+        for col in range(1,len(s1)+1):
+            if s1[col-1] == s2[row-1]:
+                matrix[row][col] = 1 + matrix[row-1][col-1]
+            else:
+                matrix[row][col] = max(matrix[row-1][col], matrix[row][col-1])
+    return matrix[len(s2)][len(s1)]
 
 def LCS(s1,s2,option):
     if option == 1:
         return LCS_recursive(s1,s2)
     else:
-        return LCS_dynamic(s1,s2)
+        matrix = [[0 for x in range(len(s1)+1)] for y in range(len(s2)+1)]
+        return LCS_dynamic(s1,s2,matrix)
 
 if __name__ == '__main__':
     s1 = 'aggtab'
